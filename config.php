19546940161994a22ed98e0ece1bd6a34ac9e8e6
unknown-o <i@mr-wu.top>
2021-02-22 14:30:12 +0800

    Version 1.2.6 released

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
-//V1.2.5版本
+//V1.2.6版本
 
 //数据库地址
 $DB_HOST="127.0.0.1";
 //数据库名
-$DB_NAME="loveway";
+$DB_NAME="BT_DB_NAME";
 //数据库登录用户名
-$DB_USER="loveway";
+$DB_USER="BT_DB_USERNAME";
 //数据库登录密码
-$DB_PASS="trmLrmj3CkpS7G62";
+$DB_PASS="BT_DB_PASSWORD";
 //管理后台账号
 $ADMIN_USER="kagamine";
 //管理后台密码
@@ -18,9 +18,6 @@ $REWRITE=false;
 /*
 伪静态规则
 当前只支持nginx，apache规则的话...实在不会写，热心的童鞋可以帮忙写一下啊（谢谢各位大佬了）
-location /
-{
-	 try_files $uri $uri/ /index.php?uri=$uri&args=$args;
-}
+
 */
 ?>
