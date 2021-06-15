22b464269ec9f91c8a3296512e194d945536bf78
吴先森 <i@mr-wu.top>
2021-06-15 16:09:41 +0800

    Version 1.3.4 released

--- pages/about.php
+++ pages/about.php
@@ -17,7 +17,7 @@ if ($templateMode) {
             <?php echo getInfo('about_content') ?>
             <br><br>
             <div class="mdui-divider"></div><br>
-            程序版本:V1.3.2<br />
+            程序版本:V1.3.4<br />
             作者博客:<a href="https://www.wunote.cn" class="text-decoration: none;">吴先森的笔记</a><br />作者邮箱:i@mr-wu.top<br>
         </div>
     </div>
