53700a6ea98ab3c7a5fa6fa9f4b539c08c29c8b6
UnknownO <i@mr-wu.top>
2022-02-11 16:17:19 +0800

    管理员登录新增验证码

--- api/admin.php
+++ api/admin.php
@@ -1,9 +1,17 @@
 <?php
 header('content-type:application/json');
+session_start();
 include('../config.php');
 include('../includes/function.php');
 switch ($_POST['mode']) {
     case "login":
+        $timestamp = $_POST['timestamp'];
+        if ($_SESSION['vcode'] != md5($_POST['vcode'] . $VERIFICATION_KEY) && $IMAGE_VERIFICATION) {
+            exit('{"code":-1,"msg":"抱歉，人机验证失败","result":""}');
+        }
+        if ($timestamp - time() > 60 || time() - $timestamp > 60) {
+            exit('{"code":-2,"msg":"请求失败！请检查您的系统时间！"}');
+        }
         if ($_POST['username'] == $ADMIN_USER && $_POST['password'] == $ADMIN_PASS) {
             setcookie("loveway_token", md5($ADMIN_USER . $ADMIN_PASS . 'KAGAMINE WORLD!' . date('Y-m-d', time())), time() + 3600, '/');
             exit('{"code":1,"msg":"success"}');
