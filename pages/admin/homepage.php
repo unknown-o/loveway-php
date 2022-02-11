8995cf63f5916d07fa92b264391ac990bb7ec27e
UnknownO <i@mr-wu.top>
2022-02-11 16:29:06 +0800

    当管理员密码没有修改时，不加载管理员页面

--- pages/admin/homepage.php
+++ pages/admin/homepage.php
@@ -10,32 +10,52 @@ if ($templateMode) {
     <h1 class="doc-chapter-title doc-chapter-title-first mdui-text-color-theme">管理页面</h1>
 </div>
 <br /><br />
-<div class="mdui-table-fluid" style="border-radius: 16px">
-    <table class="mdui-table">
-        <thead>
-            <tr>
-                <th>功能</th>
-                <th>描述</th>
-                <th>操作</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>基础设置</td>
-                <td>可以设置本站标题或其他信息</td>
-                <td><a class="mdui-btn mdui-color-theme-accent mdui-ripple" style="border-radius: 8px" href="<?php if ($REWRITE) echo "/admin/general";
-                                                                                                                else echo "/?page=admin&id=general"; ?>">去设置</a></td>
-            </tr>
-            <tr>
-                <td>表白管理</td>
-                <td>可以删除本站表白信息</td>
-                <td><a class="mdui-btn mdui-color-theme-accent mdui-ripple" style="border-radius: 8px" href="<?php if ($REWRITE) echo "/admin/confession";
-                                                                                                                else echo "/?page=admin&id=confession"; ?>">去管理</a></td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-
+<?php
+if ($ADMIN_PASS == "kagamine1234") {
+?>
+    <div class="mdui-card mdui-hoverable" style="border-radius: 16px">
+        <div class="mdui-card-media">
+            <img style="max-height: 2000px" onerror="randomImage()" src="" />
+        </div>
+        <div class="mdui-card-primary">
+            <div class="mdui-card-primary-title">抱歉！</div>
+            <div class="mdui-card-primary-subtitle">出于安全原因，管理页面被屏蔽！</div>
+        </div>
+        <div class="mdui-card-content">
+            请修改根目录中config.php的值$ADMIN_PASS后再次访问本页！<br><br>
+        </div>
+    </div>
+<?php
+} else {
+?>
+    <div class="mdui-table-fluid" style="border-radius: 16px">
+        <table class="mdui-table">
+            <thead>
+                <tr>
+                    <th>功能</th>
+                    <th>描述</th>
+                    <th>操作</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>基础设置</td>
+                    <td>可以设置本站标题或其他信息</td>
+                    <td><a class="mdui-btn mdui-color-theme-accent mdui-ripple" style="border-radius: 8px" href="<?php if ($REWRITE) echo "/admin/general";
+                                                                                                                    else echo "/?page=admin&id=general"; ?>">去设置</a></td>
+                </tr>
+                <tr>
+                    <td>表白管理</td>
+                    <td>可以删除本站表白信息</td>
+                    <td><a class="mdui-btn mdui-color-theme-accent mdui-ripple" style="border-radius: 8px" href="<?php if ($REWRITE) echo "/admin/confession";
+                                                                                                                    else echo "/?page=admin&id=confession"; ?>">去管理</a></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+<?php
+}
+?>
 <script>
     function logout() {
         setCookie('loveway_token', "kagamine yes!", -1);
