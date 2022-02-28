8612dfbcf2678e5f552214edbf91c452dc071ad8
UnknownO <i@mr-wu.top>
2022-02-28 16:15:00 +0800

    更新程序版本号

--- pages/about.php
+++ pages/about.php
@@ -18,7 +18,7 @@ if ($templateMode) {
             <br><br>
             <!-- 本程序使用GPL2.0协议开源，请遵守此协议，请勿删除本处版权，否则原作者保留一切法律权利 -->
             <div class="mdui-divider"></div><br>
-            程序版本:V1.5.7<br />
+            程序版本:V1.5.9<br />
             作者博客:<a href="https://www.wunote.cn" class="text-decoration: none;">吴先森的笔记</a><br />作者邮箱:i@mr-wu.top<br>
         </div>
     </div>
